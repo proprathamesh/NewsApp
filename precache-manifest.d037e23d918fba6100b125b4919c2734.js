ebb3c7071388621dc75ae6329c8434080e3e7716
proprathamesh <prathameshsyadav1712@gmail.com>
2023-09-12 23:08:32 +0530

    Updates

---
+++ precache-manifest.d037e23d918fba6100b125b4919c2734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d3ec9e8acd9dc579a63",
+    "url": "/NewsApp/static/css/main.99a5a1b8.chunk.css"
+  },
+  {
+    "revision": "2d3ec9e8acd9dc579a63",
+    "url": "/NewsApp/static/js/main.36c2aeac.chunk.js"
+  },
+  {
+    "revision": "119fbe6568a7262fd966",
+    "url": "/NewsApp/static/js/runtime~main.2ca99c55.js"
+  },
+  {
+    "revision": "7c32f80651e2b5a25c33",
+    "url": "/NewsApp/static/js/2.83a0ac01.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "/NewsApp/static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "d8788cb4b7ddec61e5302a5a5030ba97",
+    "url": "/NewsApp/static/media/Spinner-1s-200px.d8788cb4.gif"
+  },
+  {
+    "revision": "2f22d30c896c3fff9b543fec3f539cfa",
+    "url": "/NewsApp/index.html"
+  }
+];
